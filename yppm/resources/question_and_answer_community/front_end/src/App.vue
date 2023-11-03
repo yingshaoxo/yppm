a2ffa0c4615195b697ef8c8656ff21be18eb47ec
yingshaoxo <yingshaoxo@gmail.com>
2023-11-03 13:35:44 +0800

    try to make a basic chat view

--- yppm/resources/question_and_answer_community/front_end/src/App.vue
+++ yppm/resources/question_and_answer_community/front_end/src/App.vue
@@ -2,57 +2,61 @@
 import { Component, Vue } from 'vue-property-decorator';
 import { reactive, onMounted } from 'vue';
 
-import HelloWorld from './components/HelloWorld.vue';
+import HomeChatPage from './pages/visitor/home_chat_page.vue';
 
 import { global_dict, global_functions } from './store';
 
 @Component({
-  components: {
-    HelloWorld,
-  },
-  setup() {
-    const dict = reactive({
-        count: 0
-    })
+    components: {
+        HomeChatPage,
+    },
 
-    onMounted(() => {
-      console.log(`The count is: ${dict.count}`);
-    });
+    setup() {
+        const dict = reactive({
+        })
 
-    return {
-      global_dict,
-      global_functions,
-      dict,
-      increment() {
-        dict.count++;
-        global_functions.hi();
-      },
-    };
-  },
+        onMounted(() => {
+            console.log("hi");
+        });
+
+        return {
+            global_dict,
+            global_functions,
+            dict,
+            increment() {
+                global_functions.hi();
+            },
+        };
+    },
 })
 
 export default class App extends Vue {}
 </script>
 
 <template>
-  <div id="app">
-      <div>
+    <div id="app">
+        <div>
+            <HomeChatPage />
+        </div>
+        <!--img alt="Vue logo" src="./assets/logo.png"-->
+        <!--
         <h1>{{ global_dict.hi }}</h1>
         <h1>The count is: {{ dict.count }}</h1>
         <button @click="increment()">Increment</button>
-      </div>
-      <!--img alt="Vue logo" src="./assets/logo.png"-->
-      <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+        -->
+    </div>
 </template>
 
 <style lang="less">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+
+    margin: 0px;
+    height: 100%;
+    width: 100%;
 }
 </style>
---
+++ yppm/resources/question_and_answer_community/front_end/src/pages/visitor/home_chat_page.vue
@@ -0,0 +1,52 @@
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+
+export default class Visitor_Home_Chat_Page extends Vue {
+}
+</script>
+
+<template>
+    <div class="full_screen">
+        <div class="history_message_list">
+            <div class="message_other">Hi, yingshaoxo</div>
+            <div class="message_me">Hi, stranger.</div>
+        </div>
+        <div class="input_container _columns">
+            <input class="the_input"></input>
+            <button>Send</button>
+        </div>
+    </div>
+</template>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+.full_screen {
+    width: 100vw;
+    height: 100vh;
+}
+
+.history_message_list {
+    width: 100%;
+
+    .message_other {
+        text-align: left;
+    }
+
+    .message_me {
+        text-align: right;
+    }
+}
+
+.input_container {
+    height: 30px;
+    width: 100%;
+    justify-content: space-between;
+
+    .the_input {
+        width: 100%;
+    }
+}
+</style>
